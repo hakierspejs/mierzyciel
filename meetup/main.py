42cc8864a00c7eb0e4a73fa79b65365da463525e
Jacek Wielemborek <d33tah@gmail.com>
2020-04-17 12:22:37 +0200

    Add meetup

---
+++ meetup/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import re
+import os
+import time
+import socket
+import logging
+import urllib.parse
+
+import requests
+import lxml.html
+
+LOGGER = logging.getLogger('meetup.__main__')
+
+
+def upload_to_graphite(h, metric, value):
+    s = socket.socket()
+    try:
+        s.connect(h)
+        now = int(time.time())
+        buf = f'{metric} {value} {now}\n'.encode()
+        LOGGER.info('Sending %r to %r', buf, h)
+        s.send(buf)
+        s.close()
+    except (ConnectionRefusedError, socket.timeout) as e:
+        LOGGER.exception(e)
+    time.sleep(3.0)
+
+
+def get_num_subscribed(group_name):
+    url = 'https://www.meetup.com/' + urllib.parse.quote(group_name)
+    h = lxml.html.fromstring(requests.get(url).text)
+    return int(h.xpath('//span [contains(.,"members")]/text()')[0].split()[0])
+
+
+def main():
+    prefix = 'hakierspejs.meetup.'
+    h = ('graphite.hs-ldz.pl', 2003)
+    while True:
+        num_subscribed = get_num_subscribed('Hakierspejs-Łódź')
+        upload_to_graphite(h, prefix + 'num_subscribed', num_subscribed)
+        time.sleep(60.0)
+
+
+if __name__ == '__main__':
+    main()
