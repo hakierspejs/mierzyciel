adb909212b0aeea2189cc2bd8c52de1c98d79581
Jacek Wielemborek <d33tah@gmail.com>
2021-09-04 12:03:08 +0200

    Replace a port-forwarded service with a vhost

--- temperatura/main.py
+++ temperatura/main.py
@@ -25,7 +25,7 @@ def upload_to_graphite(h, metric, value):
 
 
 def get_degrees_celcius():
-    url = 'http://85.89.178.82:1060/'
+    url = 'http://temp.siedziba.hs-ldz.pl/'
     h = lxml.html.fromstring(requests.get(url).text)
     return float(h.xpath('//p[2]/text()')[0].strip().split(' ')[0])
 
