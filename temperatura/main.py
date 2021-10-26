deb6d2808d7d0195ebfed08727f136f60f650c30
Jacek Wielemborek <d33tah@gmail.com>
2021-10-26 20:51:27 +0200

    Dodaj obsługę aktualnego czujnika, reformat

--- temperatura/main.py
+++ temperatura/main.py
@@ -7,7 +7,7 @@ import logging
 import requests
 import lxml.html
 
-LOGGER = logging.getLogger('temperatura.__main__')
+LOGGER = logging.getLogger("temperatura.__main__")
 
 
 def upload_to_graphite(h, metric, value):
@@ -15,8 +15,8 @@ def upload_to_graphite(h, metric, value):
     try:
         s.connect(h)
         now = int(time.time())
-        buf = f'{metric} {value} {now}\n'.encode()
-        LOGGER.info('Sending %r to %r', buf, h)
+        buf = f"{metric} {value} {now}\n".encode()
+        LOGGER.info("Sending %r to %r", buf, h)
         s.send(buf)
         s.close()
     except (ConnectionRefusedError, socket.timeout) as e:
@@ -24,22 +24,27 @@ def upload_to_graphite(h, metric, value):
     time.sleep(3.0)
 
 
-def get_degrees_celcius():
-    url = 'http://temp.siedziba.hs-ldz.pl/'
-    h = lxml.html.fromstring(requests.get(url).text)
-    return float(h.xpath('//p[2]/text()')[0].strip().split(' ')[0])
+def get_response():
+    return requests.get("http://85.89.178.82:1060/cm?cmnd=STATUS+10").json()[
+        "StatusSNS"
+    ]["DHT11"]
 
 
 def main():
-    prefix = 'hakierspejs.temperatura.'
-    h = ('graphite.hs-ldz.pl', 2003)
+    prefix = "hakierspejs.temperatura."
+    h = ("graphite.hs-ldz.pl", 2003)
     while True:
-        degrees_celcius = get_degrees_celcius()
-        upload_to_graphite(h, prefix + 'degrees_celcius', degrees_celcius)
+        response = get_response()
+        upload_to_graphite(
+            h, prefix + "degrees_celcius", float(responose["Temperature"])
+        )
+        upload_to_graphite(
+            h, prefix + "humidity_percent", float(responose["Humidity"])
+        )
         time.sleep(60.0)
 
 
-if __name__ == '__main__':
-    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level='INFO', format=fmt)
+if __name__ == "__main__":
+    fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level="INFO", format=fmt)
     main()
